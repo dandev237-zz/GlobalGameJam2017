499240cd7b4c3f114d001f0f45bfda52e9c69183
Alberto <albertodelcru@gmail.com>
2017-01-20 22:41:14 +0100

    Parallax and life scripts

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Parallax : MonoBehaviour
+{
+    public float speed = 0.1f;
+    private Rect rect;
+
+    public void Awake()
+    {
+        rect = this.gameObject.GetComponent<RawImage>().uvRect;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rect.x += speed * Time.deltaTime;
+        this.GetComponent<RawImage>().uvRect = rect;
+    }
+}
