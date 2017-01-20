41399c76f2b715c02e5278a331b29a846c63cbc3
Alberto <albertodelcru@gmail.com>
2017-01-20 20:53:20 +0100

    First commit

---
+++ Assets/Scripts/MainController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainController : MonoBehaviour
+{
+    private GameObject panelHow, panelMain;
+
+    public void Awake()
+    {
+        panelMain = GameObject.Find("Canvas/PanelMain");
+        panelHow = GameObject.Find("Canvas/PanelHowToPlay");
+        panelHow.SetActive(false);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ShowHowToPlay()
+    {
+        panelMain.SetActive(false);
+        panelHow.SetActive(true);
+    }
+
+    public void BackToMain()
+    {
+        panelHow.SetActive(false);
+        panelMain.SetActive(true);
+    }
+
+}
