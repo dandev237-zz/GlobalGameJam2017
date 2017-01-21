e9c6c392803541c8b8d34483aacdaa1481cf1a36
Alberto <albertodelcru@gmail.com>
2017-01-21 12:38:47 +0100

    Player jump

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidBody;
+    private float _force = 5;
+    private bool jump = false;
+
+    public void Awake()
+    {
+        _rigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !jump)
+        {
+            jump = true;
+            _rigidBody.AddForce(Vector2.up * _force, ForceMode2D.Impulse);
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Ground"))
+        {
+            jump = false;
+        }
+    }
+}
