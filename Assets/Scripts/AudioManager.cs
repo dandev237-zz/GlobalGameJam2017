0eb2816fcff601732d009f7d122e959239cd281a
Alberto <albertodelcru@gmail.com>
2017-01-21 11:18:50 +0100

    Sprites and button prefab with effect

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioClip scream;
+
+    // Use this for initialization
+    void Awake()
+    {
+        //Load audioClips
+    }
+
+    public static AudioClip GetScream()
+    {
+        return scream;
+    }
+}
---
+++ Assets/Scripts/ButtonPressEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ButtonPressEffect : MonoBehaviour
+{
+    private Sprite pressed;
+    private Sprite button;
+    private SpriteRenderer rend;
+
+    public void Awake()
+    {
+        pressed = (Sprite)Resources.Load<Sprite>("Sprites/buttonPressed");
+        button = (Sprite)Resources.Load<Sprite>("Sprites/button");
+        rend = this.GetComponent<SpriteRenderer>();
+    }
+
+    public void OnMouseDown()
+    {
+        rend.sprite = pressed;
+    }
+
+    public void OnMouseUp()
+    {
+        rend.sprite = button;
+    }
+}
