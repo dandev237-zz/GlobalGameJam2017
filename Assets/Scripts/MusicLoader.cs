4ef45bd3953d27903921a31da07ec7b1af6d4b11
Daniel Castro García <dandev237@gmail.com>
2017-01-21 17:36:39 +0100

    MusicLoader script

---
+++ Assets/Scripts/MusicLoader.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class MusicLoader : MonoBehaviour {
+
+    public AudioSource audioSource;
+    private List<AudioClip> musicTracks;
+
+	void Start () {
+        string[] fileNames = Directory.GetFiles("Music/");
+
+        musicTracks = new List<AudioClip>();
+        foreach(string file in fileNames){
+            AudioClip clip = Resources.Load<AudioClip>("Music/" + file);
+            musicTracks.Add(clip);
+        }
+	}
+	
+	public void PlayTrack()
+    {
+        AudioClip track = musicTracks[Random.Range(0, musicTracks.Count + 1)];
+        audioSource.PlayOneShot(track);
+    }
+}
